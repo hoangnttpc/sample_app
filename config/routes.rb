989685e2507fbeb785b26e90a5f5a4ab0fd5523f
hoangnttpc <dang.quang.hoang@sun-asterisk.com>
2020-07-29 22:00:28 +0700

    Chapter 3: Mostly static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|vi/ do
-  end
+  root to: "static_pages#home"
+
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
 end
